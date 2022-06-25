6674c4635d16c15332f1c4cf5617aea3acd08b78
Yuxuan Lin <yuxuan.l@icloud.com>
2022-06-25 18:36:21 +0800

    js hack

---
+++ script.js
@@ -0,0 +1,6 @@
+const ACTIVE = 'active'
+
+const active_video = document.querySelectorAll('.main-structure .content video')
+
+active_video[0].classList.remove(ACTIVE)
+active_video[0].classList.add(ACTIVE)
