de2427a4f960694e1335b1fc4c6fc7f4cf1c00f9
Yuxuan Lin <yuxuan.l@icloud.com>
2022-06-25 21:30:37 +0800

    Lorem ipsum dolor sit amet consectetur adipisicing elit.

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ const ACTIVE = 'active'
 
 const active_video = document.querySelectorAll('.main-structure .content video')
 
+const main_header = document.querySelector('.content .text-description h1')
+const second_header = document.querySelector('.content .text-description h2')
+const paragraph = document.querySelector('.content .text-description p')
+
 active_video[0].classList.remove(ACTIVE)
 active_video[0].classList.add(ACTIVE)
+
+main_header.innerHTML = CONTENT_INFO.island.h1
+second_header.innerHTML = CONTENT_INFO.island.h2
+paragraph.innerHTML = CONTENT_INFO.island.p
